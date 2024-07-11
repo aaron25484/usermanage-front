9f2fba0ee0cc722fe57e6177085b1bda7f8393b5
Aaron de los Santos <aaronjournalist@hotmail.com>
2024-07-11 02:54:00 +0200

    fix: url's services

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -9,13 +9,13 @@ interface IUser {
 const { VITE_API_URL } = import.meta.env
 
 const register = async (user: IUser) => {
-  const response = await axios.post(`${VITE_API_URL}/users`, user);
+  const response = await axios.post(`${VITE_API_URL}users`, user);
   console.log(response);
   return response.data;
 };
 
 const login = async (email: string, password: string) => {
-  const response = await axios.post(`${VITE_API_URL}/users/login`, { email, password });
+  const response = await axios.post(`${VITE_API_URL}users/login`, { email, password });
   return response.data;
 };
 
@@ -25,18 +25,18 @@ const list = async () => {
 };
 
 const deleteUser = async (id: string) => {
-  const response = await axios.delete(`${VITE_API_URL}/users/${id}`);
+  const response = await axios.delete(`${VITE_API_URL}users/${id}`);
   return response.data;
 };
 
 const addFriend = async (userId: string, friendId: string) => {
-  const response = await axios.put(`${VITE_API_URL}/users/${userId}/friends/${friendId}`);
+  const response = await axios.put(`${VITE_API_URL}users/${userId}/friends/${friendId}`);
   return response.data;
 };
 
 const removeFriend = async (userId: string, friendId: string) => {
   try {
-    const response = await axios.delete(`${VITE_API_URL}/users/${userId}/friends/${friendId}`);
+    const response = await axios.delete(`${VITE_API_URL}users/${userId}/friends/${friendId}`);
     return response.data;
   } catch (error) {
     console.error('Failed to remove friend:', error);
@@ -45,7 +45,7 @@ const removeFriend = async (userId: string, friendId: string) => {
 };
 
 const updateUser = async (id: string, newName: string) => {
-  const response = await axios.put(`${VITE_API_URL}/users/${id}`, { name: newName });
+  const response = await axios.put(`${VITE_API_URL}users/${id}`, { name: newName });
   return response.data;
 };
 
