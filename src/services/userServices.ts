4da4cbcfedc1e328554a1d0c760ed986a174f66b
Aaron de los Santos <aaronjournalist@hotmail.com>
2024-07-11 02:48:45 +0200

    fix: url's

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -9,34 +9,34 @@ interface IUser {
 const { VITE_API_URL } = import.meta.env
 
 const register = async (user: IUser) => {
-  const response = await axios.post(`${VITE_API_URL}`, user);
+  const response = await axios.post(`${VITE_API_URL}/users`, user);
   console.log(response);
   return response.data;
 };
 
 const login = async (email: string, password: string) => {
-  const response = await axios.post(`${VITE_API_URL}/login`, { email, password });
+  const response = await axios.post(`${VITE_API_URL}/users/login`, { email, password });
   return response.data;
 };
 
 const list = async () => {
-  const response = await axios.get(VITE_API_URL);
+  const response = await axios.get(`${VITE_API_URL}/users`);
   return response.data;
 };
 
 const deleteUser = async (id: string) => {
-  const response = await axios.delete(`${VITE_API_URL}/${id}`);
+  const response = await axios.delete(`${VITE_API_URL}/users/${id}`);
   return response.data;
 };
 
 const addFriend = async (userId: string, friendId: string) => {
-  const response = await axios.put(`${VITE_API_URL}/${userId}/friends/${friendId}`);
+  const response = await axios.put(`${VITE_API_URL}/users/${userId}/friends/${friendId}`);
   return response.data;
 };
 
 const removeFriend = async (userId: string, friendId: string) => {
   try {
-    const response = await axios.delete(`${VITE_API_URL}/${userId}/friends/${friendId}`);
+    const response = await axios.delete(`${VITE_API_URL}/users/${userId}/friends/${friendId}`);
     return response.data;
   } catch (error) {
     console.error('Failed to remove friend:', error);
@@ -45,7 +45,7 @@ const removeFriend = async (userId: string, friendId: string) => {
 };
 
 const updateUser = async (id: string, newName: string) => {
-  const response = await axios.put(`${VITE_API_URL}/${id}`, { name: newName });
+  const response = await axios.put(`${VITE_API_URL}/users/${id}`, { name: newName });
   return response.data;
 };
 
